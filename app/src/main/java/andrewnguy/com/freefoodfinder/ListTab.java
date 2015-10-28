c4944aaed8f04ebb0cf3ad31910eb5b31eaf5a0d
anguy95 <anguy95@gmail.com>
2015-10-28 01:04:34 -0700

    Launcher Icon. Subject to change

--- app/src/main/java/andrewnguy/com/freefoodfinder/ListTab.java
+++ app/src/main/java/andrewnguy/com/freefoodfinder/ListTab.java
@@ -11,11 +11,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ListTab extends Fragment {
+
+    ArrayList<String> animalsNameList;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.list_tab,container,false);
+        View v = inflater.inflate(R.layout.list_tab, container, false);
 
         FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.list_fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +41,61 @@ public class ListTab extends Fragment {
         });
 
 
+        ListView animalList = (ListView) v.findViewById(R.id.listView);
+
+
+
+        animalsNameList = new ArrayList<String>();
+        getAnimalNames();
+        // Create The Adapter with passing ArrayList as 3rd parameter
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.mytextview, animalsNameList);
+        // Set The Adapter
+        animalList.setAdapter(arrayAdapter);
+
+        // register onClickListener to handle click events on each item
+        animalList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // argument position gives the index of item which is clicked
+            public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3) {
+
+                String selectedAnimal = animalsNameList.get(position);
+                Toast.makeText(getActivity().getApplicationContext(), "Animal Selected : " + selectedAnimal, Toast.LENGTH_LONG).show();
+            }
+        });
+
         return v;
+    }
+
+    void getAnimalNames()
+    {
+        animalsNameList.add("DOG");
+        animalsNameList.add("CAT");
+        animalsNameList.add("HORSE");
+        animalsNameList.add("ELEPHANT");
+        animalsNameList.add("LION");
+        animalsNameList.add("COW");
+        animalsNameList.add("MONKEY");
+        animalsNameList.add("DEER");
+        animalsNameList.add("RABBIT");
+        animalsNameList.add("BEER");
+        animalsNameList.add("DONKEY");
+        animalsNameList.add("LAMB");
+        animalsNameList.add("GOAT");
+        animalsNameList.add("DOG");
+        animalsNameList.add("CAT");
+        animalsNameList.add("HORSE");
+        animalsNameList.add("ELEPHANT");
+        animalsNameList.add("LION");
+        animalsNameList.add("COW");
+        animalsNameList.add("MONKEY");
+        animalsNameList.add("DEER");
+        animalsNameList.add("RABBIT");
+        animalsNameList.add("BEER");
+        animalsNameList.add("DONKEY");
+        animalsNameList.add("LAMB");
+        animalsNameList.add("GOAT");
+
+
+
     }
 }
