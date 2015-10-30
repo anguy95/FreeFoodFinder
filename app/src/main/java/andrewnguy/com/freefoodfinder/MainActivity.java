3a8673bb1ef94e8b26637738998015e4ae754d91
Mac <randy.tran11@gmail.com>
2015-10-30 16:14:29 -0700

    Parse library. Event basic functions. Cleaned up code

--- app/src/main/java/andrewnguy/com/freefoodfinder/MainActivity.java
+++ app/src/main/java/andrewnguy/com/freefoodfinder/MainActivity.java
@@ -1,15 +1,8 @@
 package andrewnguy.com.freefoodfinder;
-//hi
-import android.content.Context;
-import android.content.Intent;
-import android.location.LocationManager;
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -29,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
         // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
         adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
