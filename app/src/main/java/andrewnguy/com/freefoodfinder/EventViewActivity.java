f42c2aa7cfa561c24b7d75ef081a687833669daa
anguy95 <anguy95@gmail.com>
2015-11-07 22:37:47 -0800

    Fixed coloring of the FAB

--- app/src/main/java/andrewnguy/com/freefoodfinder/EventViewActivity.java
+++ app/src/main/java/andrewnguy/com/freefoodfinder/EventViewActivity.java
@@ -3,6 +3,8 @@ package andrewnguy.com.freefoodfinder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Class that allows us the see an event in detail
@@ -10,16 +12,30 @@ import android.os.Bundle;
  * or clicking on "more information" from the MapsTab
  */
 public class EventViewActivity extends AppCompatActivity {
-
     private Intent intent;
+    private android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_view);
 
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
         intent = getIntent(); // get intent that this came from: needs to know to go back to the Maps or List
+
     }
+
+
 
     @Override
     protected void onDestroy() {
